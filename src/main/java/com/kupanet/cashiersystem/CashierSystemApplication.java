acbc06443f4630139012575e47940ed42edac004
tiyh <actiyh@163.com>
2019-12-01 20:00:42 +0800

    fix SpringBoot error:Velocity is missing

--- src/main/java/com/kupanet/cashiersystem/CashierSystemApplication.java
+++ src/main/java/com/kupanet/cashiersystem/CashierSystemApplication.java
@@ -3,9 +3,11 @@ package com.kupanet.cashiersystem;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.velocity.VelocityAutoConfiguration;
 
-@SpringBootApplication
+
 @MapperScan("com.kupanet.cashiersystem.DAO")
+@SpringBootApplication(exclude = VelocityAutoConfiguration.class)
 public class CashierSystemApplication {
 
 	public static void main(String[] args) {
